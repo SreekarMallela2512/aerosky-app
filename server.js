9d22d8d341f2996da47aa8e89e03bdc60fd7d688
SreekarMallela <sreekarmallela2512@gmail.com>
2025-09-14 22:28:48 +0530

    serverchange3

--- server.js
+++ server.js
@@ -35,8 +35,6 @@ const connectDB = async () => {
     }
 
     await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       serverSelectionTimeoutMS: 10000,
       socketTimeoutMS: 45000,
     });
